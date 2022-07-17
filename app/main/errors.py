f091fa5c911f28542687b5ed4caedfb58e549939
A5KET <asketinin@gmail.com>
2022-07-17 12:24:43 +0300

    Error templates moved to a separate folder

--- app/main/errors.py
+++ app/main/errors.py
@@ -6,9 +6,9 @@ from . import main
 @main.app_errorhandler(404)
 def page_not_found(error):
     print(error)
-    return flask.render_template('404.html'), 404
+    return flask.render_template('errors/404.html'), 404
 
 
 @main.app_errorhandler(500)
 def internal_server_error(e):
-    return flask.render_template('500.html'), 500
+    return flask.render_template('errors/500.html'), 500
