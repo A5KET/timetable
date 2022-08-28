ad11f8192b1dae74aaaf9ab52eeef4e8f959e9b2
A5KET <asketinin@gmail.com>
2022-08-28 19:58:29 +0300

    Design rework

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,7 +2,7 @@ import flask
 import flask_login
 from flask_login import current_user
 
-from .forms import SignUp, Login
+from .forms import SignUp, SignIn
 from app import db, models, login_manager
 
 from . import auth
@@ -13,28 +13,27 @@ def load_user(user_id):
     return db.session.get(models.User, user_id)
 
 
-@auth.route('/login', methods=['GET', 'POST'])
-def login():
-    form = Login()
+@auth.route('/signin', methods=['GET', 'POST'])
+def signin():
+    form = SignIn()
 
     if current_user.is_authenticated:  # type: ignore
         return flask.redirect(flask.url_for('auth.profile'))
 
     if form.validate_on_submit():
-        username = form.username.data
-        password = form.password.data
+        email: str = form.email.data
+        password: str = form.password.data
 
-        user = db.session.query(models.User).filter(models.User.username == username, models.User.password == password).first()
+        user: models.User | None = models.User.query.filter_by(email=email.lower()).first()
         print(user)
 
-        if not user:
-            flask.flash('Username or password is invalid')
-            return flask.redirect(flask.url_for('auth.login'))
+        if user and user.verify_password(password):
+            flask_login.login_user(user)
+            return flask.redirect(flask.url_for('main.index'))
 
-        flask_login.login_user(user)
-        return flask.redirect(flask.url_for('main.index'))
+        flask.flash('Username or password is invalid')
 
-    return flask.render_template('auth/login.html', form=form)
+    return flask.render_template('auth/signin.html', form=form)
 
 
 @auth.route('/logout')
